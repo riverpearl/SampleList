fc615c8c4fd35ee91e681a9a2983d989a8f1d885
River Pearl <riverpearl0115@gmail.com>
2016-07-13 16:37:51 +0900

    image click listener

--- app/src/main/java/com/tacademy/samplelist/CustomListActivity.java
+++ app/src/main/java/com/tacademy/samplelist/CustomListActivity.java
@@ -3,9 +3,12 @@ package com.tacademy.samplelist;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.tacademy.samplelist.data.Person;
+import com.tacademy.samplelist.widget.PersonView;
 
 import java.util.Random;
 
@@ -13,6 +16,7 @@ public class CustomListActivity extends AppCompatActivity {
 
     private ListView listView;
     private PersonAdapter mAdapter;
+    private ImageView pictureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,26 @@ public class CustomListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_custom_list);
 
         listView = (ListView)findViewById(R.id.listView3);
+
+        //커진 사진을 누르면 안 보이게 한다.
+        pictureView = (ImageView)findViewById(R.id.image_picture);
+        pictureView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pictureView.setVisibility(View.GONE);
+            }
+        });
+
+        //아답터에 담겨있는 사진을 누르면 큰 사진이 나타난다.
         mAdapter = new PersonAdapter();
+        mAdapter.setOnAdapterImageClickListener(new PersonAdapter.OnAdapterImageClickListener() {
+            @Override
+            public void onAdapterImageClick(PersonAdapter adapter, PersonView view, Person person) {
+                pictureView.setImageDrawable(person.getPhoto());
+                pictureView.setVisibility(View.VISIBLE);
+            }
+        });
+
         listView.setAdapter(mAdapter);
         initData();
     }
--- app/src/main/java/com/tacademy/samplelist/PersonAdapter.java
+++ app/src/main/java/com/tacademy/samplelist/PersonAdapter.java
@@ -13,7 +13,7 @@ import java.util.List;
 /**
  * Created by Tacademy on 2016-07-13.
  */
-public class PersonAdapter extends BaseAdapter {
+public class PersonAdapter extends BaseAdapter implements PersonView.OnImageClickListener {
     List<Person> items = new ArrayList<>();
 
     public void add(Person p) {
@@ -50,11 +50,29 @@ public class PersonAdapter extends BaseAdapter {
     public View getView(int position, View converView, ViewGroup parent) {
         PersonView view;
 
-        if (converView == null)
+        if (converView == null) {
             view = new PersonView(parent.getContext());
+            view.setOnImageClickListenr(this);
+        }
         else view = (PersonView)converView;
 
         view.setPerson(items.get(position));
         return view;
+    }
+
+    public interface OnAdapterImageClickListener {
+        public void onAdapterImageClick(PersonAdapter adapter, PersonView view, Person person);
+    }
+
+    OnAdapterImageClickListener mAdapterListener;
+    public void setOnAdapterImageClickListener(OnAdapterImageClickListener listener) {
+        mAdapterListener = listener;
+    }
+
+    @Override
+    public void onImageClick(PersonView view, Person person) {
+        if (mAdapterListener != null) {
+            mAdapterListener.onAdapterImageClick(this, view, person);
+        }
     }
 }
